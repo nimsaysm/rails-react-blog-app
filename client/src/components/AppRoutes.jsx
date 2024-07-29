9dfc92b7d1dac079775ff891cc2bab8c7b8e933f
nimsaysm <yasmin.soraya14@gmail.com>
2024-07-29 10:06:55 -0300

    Add: Edit functionality

--- client/src/components/AppRoutes.jsx
+++ client/src/components/AppRoutes.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import PostsList from "../features/posts/PostsList";
 import PostDetails from "../features/posts/PostDetails";
 import NewPostForm from "../features/posts/NewPostForm";
+import PostEditForm from "../features/posts/PostEditForm";
 
 function AppRoutes() {
     return (
@@ -10,6 +11,7 @@ function AppRoutes() {
             <Route path="/" element={<PostsList />} />
             <Route path="/new" element={<NewPostForm />} />
             <Route path="posts/:id" element={<PostDetails />} />
+            <Route path="posts/:id/edit" element={<PostEditForm />} />
         </Routes>
     )
 }

27a19d4083a271c061260026865bea7b8718c6af
nimsaysm <yasmin.soraya14@gmail.com>
2024-08-05 10:46:43 -0300

    Editing comments

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # * -> accept all requests
-    origins "*" #accept only vite URL requests
+    origins "*" 
 
     resource "*",
       headers: :any,
